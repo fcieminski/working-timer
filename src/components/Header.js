e7dacd38cca80e1bda2b6ce35116e370f2fcf5ef
Filip <f.cieminski@gmail.com>
2019-04-30 15:31:05 +0200

    first few code lines just to make a header and main screen, also, implementation of scss

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+function Header() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__menu">
+        <p className="menu__element">Object</p>
+        <p className="menu__element">Object</p>
+        <p className="menu__element">Object</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/MainScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+function MainScreen() {
+  return (
+    <div>
+      <Header />
+      <div className="main">
+        <h1>Hello!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainScreen;
