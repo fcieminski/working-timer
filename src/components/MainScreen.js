bf3aa2319e241bd31083a086633a9323e4ae58e9
Filip <f.cieminski@gmail.com>
2019-05-12 23:15:47 +0200

    main screen in progress

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -8,11 +8,31 @@ const MainScreen = () => {
   return (
     <div>
       <Header />
-      <div className="main">
-        <TimeCounter />
-        <TimeTracker />
-        <WorkCountdown />
-      </div>
+      <main className="main">
+        <section className="main__container">
+          <div className="container__herotext">
+            <h1>Lorem import from "module";</h1>
+          </div>
+          <div className="container__heroimage">
+            <h1>Lorem import import from " sd";</h1>
+          </div>
+        </section>
+        <div className="container__line" />
+        <section className="main__container--features">
+          <div className="features__box">
+            <h2>hello heloo</h2>
+            <p>eloelohelo</p>
+          </div>
+          <div className="features__box">
+            <h2>hello heloo</h2>
+            <p>eloelohelo</p>
+          </div>
+          <div className="features__box">
+            <h2>hello heloo</h2>
+            <p>eloelohelo</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
