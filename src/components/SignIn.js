570df8b59300f5678239d8712103c95eca89dfe5
Filip <f.cieminski@gmail.com>
2019-05-17 00:37:09 +0200

    hooks in signin component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
+import firebase from "firebase";
 
 const SignIn = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <>
       <form>
@@ -11,6 +15,7 @@ const SignIn = () => {
           label="imię"
           value={name}
           required
+          onChange={() => setName(event.target.value)}
         />
         <TextField
           placeholder="e-mail"
@@ -18,6 +23,7 @@ const SignIn = () => {
           label="e-mail"
           value={email}
           required
+          onChange={() => setEmail(event.target.value)}
         />
         <TextField
           placeholder="hasło"
@@ -25,6 +31,7 @@ const SignIn = () => {
           label="hasło"
           value={password}
           required
+          onChange={() => setPassword(event.target.value)}
         />
         <button type="submit">Zarejestruj się</button>
       </form>
